8dedc3cbc156df58fc13f9a073683eb06149c804
James Page <james.page@ubuntu.com>
2022-09-12 15:13:09 +0100

    [docs] fix missing trailing ,

--- lib/charms/openstack_libs/v0/keystone_requires.py
+++ lib/charms/openstack_libs/v0/keystone_requires.py
@@ -47,7 +47,7 @@ class IdentityServiceClientCharm(CharmBase):
                 "internal_url": "http://internal-url",
                 "public_url": "http://public-url",
                 "admin_url": "http://admin-url",
-            }]
+            }],
             region = "region"
         )
         self.framework.observe(
@@ -105,7 +105,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 2
+LIBPATCH = 3
 
 
 class KeystoneConnectedEvent(EventBase):
