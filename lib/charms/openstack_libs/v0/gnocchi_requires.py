3bbf61675b62cb0daf16e589ba6986f2fb032f16
hernandanielg <hernan.garcia@canonical.com>
2022-09-19 22:37:17 +0000

    gnocchi libraryinitial version

--- lib/charms/openstack_libs/v0/gnocchi_requires.py
+++ lib/charms/openstack_libs/v0/gnocchi_requires.py
@@ -1,23 +1,25 @@
+# Copyright 2022 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """TODO: Add a proper docstring here.
-
-This is a placeholder docstring for this charm library. Docstrings are
-presented on Charmhub and updated whenever you push a new version of the
-library.
-
-Complete documentation about creating and documenting libraries can be found
-in the SDK docs at https://juju.is/docs/sdk/libraries.
-
-See `charmcraft publish-lib` and `charmcraft fetch-lib` for details of how to
-share and consume charm libraries. They serve to enhance collaboration
-between charmers. Use a charmer's libraries for classes that handle
-integration with their charm.
-
-Bear in mind that new revisions of the different major API versions (v0, v1,
-v2 etc) are maintained independently.  You can continue to update v0 and v1
-after you have pushed v3.
-
-Markdown is supported, following the CommonMark specification.
 """
+
+import json
+import logging
+
+from ops.framework import EventBase, EventSource, Object, ObjectEvents
+from ops.model import Relation
 
 # The unique Charmhub library identifier, never change it
 LIBID = "bdc4aef454524b6eaa90501b3c9d500c"
@@ -29,4 +31,91 @@ LIBAPI = 0
 # to 0 if you are raising the major API version
 LIBPATCH = 1
 
-# TODO: add your code here! Happy coding!
+
+class GnocchiConnectedEvent(EventBase):
+    """Gnocchi connected Event."""
+    pass
+
+
+class GnocchiReadyEvent(EventBase):
+    """Gnocchi ready for use Event."""
+    pass
+
+
+class GnocchiGoneAwayEvent(EventBase):
+    """Gnocchi relation has gone-away Event."""
+    pass
+
+
+class GnocchiServerEvents(ObjectEvents):
+    """Events class for `on`."""
+
+    connected = EventSource(GnocchiConnectedEvent)
+    ready = EventSource(GnocchiReadyEvent)
+    goneaway = EventSource(GnocchiGoneAwayEvent)
+
+
+class GnocchiRequires(Object):
+    """Requires side interface for gnocchi interface type."""
+
+    on = GnocchiServerEvents()
+
+    def __init__(
+        self, charm, relation_name: str
+    ):
+        super().__init__(charm, relation_name)
+        self.charm = charm
+        self.relation_name = relation_name
+
+        self.framework.observe(
+            self.charm.on[relation_name].relation_joined,
+            self._on_gnocchi_relation_joined,
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_changed,
+            self._on_gnocchi_relation_changed,
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_departed,
+            self._on_gnocchi_relation_changed,
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_broken,
+            self._on_gnocchi_relation_broken,
+        )
+
+    def _on_gnocchi_relation_joined(self, event):
+        """Gnocchi relation joined."""
+        logging.debug("Gnocchi on_joined")
+        self.on.connected.emit()
+
+    def _on_gnocchi_relation_changed(self, event):
+        """Gnocchi relation changed."""
+        logging.debug("Gnocchi on_changed")
+        self.on.ready.emit()
+
+    def _on_gnocchi_relation_broken(self, event):
+        """Gnocchi relation broken."""
+        logging.debug("Gnocchi on_broken")
+        self.on.goneaway.emit()
+
+    @property
+    def _gnocchi_rel(self) -> Relation:
+        """The Gnocchi relation."""
+        return self.framework.model.get_relation(self.relation_name)
+
+    def _get_remote_unit_data(self, key: str) -> str:
+        """Return the value for the given key from remote app data."""
+        for unit in self._gnocchi_rel.units:
+            data = self._gnocchi_rel.data[unit]
+            return data.get(key)
+
+    def get_data(self, key: str) -> str:
+        """Return the value for the given key."""
+        return self._get_remote_unit_data(key)
+
+    @property
+    def gnocchi_url(self) -> str:
+        """Return the gnocchi_url."""
+        return self.get_data("gnocchi_url")
+
